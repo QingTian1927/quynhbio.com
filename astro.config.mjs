169f302f3ab749ec801428fd1d3101e776177b1d
QingTian <qingtian63910@proton.me>
2024-04-16 16:03:31 +0700

    feat: updated astro config file.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 
+import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue()]
+  integrations: [tailwind(), vue()],
+  site: "https://www.quynhbio.com",
+  server: { port: 6969, host: true },
 });
