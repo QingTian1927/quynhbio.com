3eeaf66b4a7efa8ea0ea085d959189803511ff36
QingTian <qingtian63910@proton.me>
2024-04-19 21:09:36 +0700

    refactor(i18n): added footer translations.
    * Updated the footer links in the footer component to use translated text.

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -5,6 +5,8 @@ export const languages = {
 
 export const defaultLang = 'vi';
 
+export const currentYear = new Date().getFullYear();
+
 export const ui = {
     en: {
         'nav.home': 'Home',
@@ -12,6 +14,11 @@ export const ui = {
         'nav.blog': 'Blog',
         'nav.about': 'About',
         'nav.contact': 'Contact',
+
+        'footer.copyright': `Copyright © ${currentYear} QuynhBio. All rights reserved.`,
+        'footer.sitemap': 'Site Map',
+        'footer.privacy': 'Privacy Policy',
+        'footer.terms': 'Terms of Service',
     },
 
     vi: {
@@ -20,5 +27,10 @@ export const ui = {
         'nav.blog': 'Tin Tức',
         'nav.about': 'Giới Thiệu',
         'nav.contact': 'Liên Hệ',
+
+        'footer.copyright': `Bản quyền © ${currentYear} QuynhBio. Mọi quyền được bảo lưu.`,
+        'footer.sitemap': 'Sơ đồ Website',
+        'footer.privacy': 'Chính sách quyền riêng tư',
+        'footer.terms': 'Điều khoản sử dụng',
     },
 } as const;
