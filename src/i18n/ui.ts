cbe954ddace6c94ef7cc7c6ef4955c97f082402e
QingTian <qingtian63910@proton.me>
2024-04-19 22:45:21 +0700

    refactor(i18n): updated home hero section.
    * Update text in the home hero section to use translations.

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -19,6 +19,14 @@ export const ui = {
         'footer.sitemap': 'Site Map',
         'footer.privacy': 'Privacy Policy',
         'footer.terms': 'Terms of Service',
+
+        'home.hero.title.firstHalf': 'Become more',
+        'home.hero.title.beautiful': 'beautiful',
+        'home.hero.title.preBreak': ' ',
+        'home.hero.title.secondHalf': 'with Quỳnh Bio',
+        'home.hero.subtitle': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magnam aliquam quaerat.',
+        'home.hero.shop-now': 'Shop Now',
+        'home.hero.contact-us': 'Contact Us',
     },
 
     vi: {
@@ -32,5 +40,13 @@ export const ui = {
         'footer.sitemap': 'Sơ đồ Website',
         'footer.privacy': 'Chính sách quyền riêng tư',
         'footer.terms': 'Điều khoản sử dụng',
+
+        'home.hero.title.firstHalf': 'Trở nên',
+        'home.hero.title.beautiful': 'xinh đẹp',
+        'home.hero.title.preBreak': 'hơn',
+        'home.hero.title.secondHalf': 'với Quỳnh Bio',
+        'home.hero.subtitle': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magnam aliquam quaerat.',
+        'home.hero.shop-now': 'Mua Hàng Ngay',
+        'home.hero.contact-us': 'Liên Hệ Với Chúng Tôi',
     },
 } as const;
