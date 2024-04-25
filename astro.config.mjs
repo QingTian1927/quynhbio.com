6d1236f1f67183ff7c213b7c17d68a23fdfec64d
QingTian <qingtian63910@proton.me>
2024-04-25 14:22:22 +0700

    chore: added node SSR adapter.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 import db from "@astrojs/db";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,5 +19,9 @@ export default defineConfig({
   i18n: {
     defaultLocale: "vi",
     locales: ["en", "vi"]
-  }
+  },
+  output: "hybrid",
+  adapter: node({
+    mode: "standalone"
+  })
 });
