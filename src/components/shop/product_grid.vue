28137c74230b589cb2df27578977b628a1586085
QingTian <qingtian63910@proton.me>
2024-04-26 14:04:01 +0700

    fix: added missing light theme  for mobile components.

--- src/components/shop/product_grid.vue
+++ src/components/shop/product_grid.vue
@@ -27,7 +27,7 @@ onMounted(() => {
 			<div class="w-full lg:hidden flex items-center justify-center">
 				<div class="w-full h-auto px-3.5 py-2.5 flex items-center justify-center">
 					<button ref="asideOpenButton"
-						class="group w-full px-3.5 py-2.5 rounded-md flex items-center justify-center dark:bg-stone-700 hover:dark:bg-red-400">
+						class="group w-full px-3.5 py-2.5 rounded-md flex items-center justify-center bg-orange-200 hover:bg-red-300 dark:bg-stone-700 hover:dark:bg-red-400">
 						<div class="flex items-center justify-center mr-2">
 							<svg xmlns="http://www.w3.org/2000/svg" width="1.2rem" height="1.2rem"
 								class="bi bi-sliders fill-stone-800 dark:fill-orange-100 group-hover:fill-stone-800"
@@ -42,7 +42,7 @@ onMounted(() => {
 			</div>
 
 			<aside ref="aside"
-				class="hidden lg:block w-full h-full sm:w-1/2 md:w-1/3 sm:h-full lg:w-[22.5%] lg:h-auto px-8 py-10 lg:px-3.5 lg:py-2.5 text-left fixed lg:sticky left-0 right-0 bottom-0 lg:top-0 sm:border-r-[3px] lg:border-none border-orange-200 dark:border-stone-700 bg-stone-800 lg:bg-transparent">
+				class="hidden lg:block w-full h-full sm:w-1/2 md:w-1/3 sm:h-full lg:w-[22.5%] lg:h-auto px-8 py-10 lg:px-3.5 lg:py-2.5 text-left fixed lg:sticky left-0 right-0 bottom-0 lg:top-0 sm:border-r-[3px] lg:border-none border-orange-200 dark:border-stone-700 bg-orange-100 dark:bg-stone-800 lg:bg-transparent">
 				<div class="flex items-center justify-between lg:justify-start">
 					<h2 class="inline font-bold text-2xl leading-6">Bộ Lọc</h2>
 					<button ref="asideCloseButton"
