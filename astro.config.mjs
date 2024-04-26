f5b75736c652f380642c7d403678d18354ae58d6
QingTian <qingtian63910@proton.me>
2024-04-26 13:32:52 +0700

    refactor: formatted astro.config.mjs a bit.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 import db from "@astrojs/db";
@@ -8,20 +9,24 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue(), db()],
+
   site: "https://www.quynhbio.com",
   server: {
     port: 6969,
     host: true
   },
+
   markdown: {
     syntaxHighlight: false
   },
+
   i18n: {
     defaultLocale: "vi",
     locales: ["en", "vi"]
   },
+
   output: "hybrid",
   adapter: node({
     mode: "standalone"
-  })
+  }),
 });
