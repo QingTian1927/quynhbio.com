cdfe1ad3758cd38be0848e4d0f68bf8a2f06de9c
QingTian <qingtian63910@proton.me>
2024-04-27 21:20:10 +0700

    feat: added light theme support for the image picker.

--- src/components/imageToBlob/image_picker.vue
+++ src/components/imageToBlob/image_picker.vue
@@ -102,35 +102,35 @@ onMounted(() => {
 <template>
 	<div class="w-full flex items-center justify-center">
 		<div class="w-full lg:w-4/5 px-8 py-10 flex flex-col items-center justify-center">
-			<h2 class="px-3.5 mb-2.5 self-start font-bold text-xl">Choose an image</h2>
+			<h2 class="px-3.5 mb-2.5 self-start font-bold text-xl text-stone-800 dark:text-orange-100">Choose an image</h2>
 			<div class="w-full px-3.5 py-2.5 flex items-center justify-between">
 				<label ref="chosenFileLabel" for="imagePicker"
-					class="grow px-3.5 py-2.5 mr-2.5 line-clamp-2 text-ellipsis overflow-hidden rounded-md dark:bg-stone-700 dark:text-orange-100/50 cursor-pointer">
+					class="grow px-3.5 py-2.5 mr-2.5 line-clamp-2 text-ellipsis overflow-hidden rounded-md bg-orange-200 text-stone-800/50 dark:bg-stone-700 dark:text-orange-100/50 cursor-pointer">
 					{{ noImageText }}
 				</label>
 
 				<label for="imagePicker"
-					class="self-stretch px-3.5 py-2.5 flex items-center justify-center font-semibold rounded-md dark:bg-stone-700 hover:bg-red-300 dark:hover:text-stone-800 dark:hover:bg-red-400 cursor-pointer">Browse</label>
+					class="self-stretch px-3.5 py-2.5 flex items-center justify-center font-semibold rounded-md bg-orange-200 dark:bg-stone-700 hover:bg-red-300 dark:hover:text-stone-800 dark:hover:bg-red-400 cursor-pointer">Browse</label>
 				<input ref="imagePicker" type="file" accept="image/*" id="imagePicker" name="imagePicker"
 					class="hidden" />
 			</div>
 
 			<div class="w-full px-3.5">
-				<hr class="w-full border-stone-700 my-5">
+				<hr class="w-full border-stone-800/50 dark:border-stone-700 my-5">
 			</div>
 
-			<h2 class="px-3.5 mb-2.5 self-start font-bold text-xl">Image preview</h2>
+			<h2 class="px-3.5 mb-2.5 self-start font-bold text-xl text-stone-800 dark:text-orange-100">Image preview</h2>
 			<div class="w-full aspect-[2/1] mb-5 px-3.5 py-2.5 flex items-center justify-center">
-				<div class="w-full h-full p-3.5 rounded-md flex flex-col items-center justify-center dark:bg-stone-700">
+				<div class="w-full h-full p-3.5 rounded-md flex flex-col items-center justify-center bg-orange-200 dark:bg-stone-700">
 					<div class="w-full h-full flex flex-col items-center justify-center"
 						:class="{ hidden: selectedImage }">
 						<svg xmlns="http://www.w3.org/2000/svg" width="15%" height="15%"
-							class="bi bi-image dark:fill-orange-100/50" viewBox="0 0 16 16">
+							class="bi bi-image fill-stone-800/50 dark:fill-orange-100/50" viewBox="0 0 16 16">
 							<path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0" />
 							<path
 								d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1z" />
 						</svg>
-						<p class="mt-2 dark:text-orange-100/50">No image selected</p>
+						<p class="mt-2 text-stone-800/50 dark:text-orange-100/50">No image selected</p>
 					</div>
 
 					<div class="w-full h-full flex flex-col items-center justify-center"
@@ -140,21 +140,21 @@ onMounted(() => {
 				</div>
 			</div>
 
-			<h2 class="px-3.5 mb-2.5 self-start font-bold text-xl">Data URL contents</h2>
+			<h2 class="px-3.5 mb-2.5 self-start font-bold text-xl text-stone-800 dark:text-orange-100">Data URL contents</h2>
 			<div class="w-full px-3.5 mb-5 flex items-center justify-center">
 				<textarea ref="outputTextArea" name="outputTextArea" readonly
-					class="w-full min-h-64 p-3.5 rounded-md text-xs font-mono dark:bg-stone-700 dark:text-orange-100 dark:placeholder:text-orange-100/50"
+					class="w-full min-h-64 p-3.5 rounded-md text-xs font-mono bg-orange-200 text-stone-800 placeholder:text-stone-800/50 dark:bg-stone-700 dark:text-orange-100 dark:placeholder:text-orange-100/50"
 					placeholder="No image selected. Select one by using the file picker above."></textarea>
 			</div>
 
 			<div class="w-full px-3.5 flex items-center justify-center">
 				<button ref="copyClipboardButton"
-					class="self-stretch px-3.5 py-2.5 mr-2.5 font-semibold rounded-md dark:bg-stone-700 dark:hover:bg-red-400 dark:hover:text-stone-800">
+					class="self-stretch px-3.5 py-2.5 mr-2.5 font-semibold rounded-md bg-orange-200 hover:bg-red-300 dark:bg-stone-700 dark:hover:bg-red-400 dark:hover:text-stone-800">
 					Copy to clipboard
 				</button>
 
 				<a ref="downloadButton"
-					class="self-stretch cursor-pointer px-3.5 py-2.5 font-semibold rounded-md dark:bg-stone-700 dark:hover:bg-red-400 dark:hover:text-stone-800">
+					class="self-stretch cursor-pointer px-3.5 py-2.5 font-semibold rounded-md bg-orange-200 hover:bg-red-300 dark:bg-stone-700 dark:hover:bg-red-400 dark:hover:text-stone-800">
 					Download as text file
 				</a>
 			</div>
