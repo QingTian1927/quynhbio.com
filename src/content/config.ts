fe69a6c63393036abc00a35a4d58d006309cfc4c
QingTian <qingtian63910@proton.me>
2024-04-29 17:00:54 +0700

    feat: expanded the blog collection schema.
    * Expanded the blog collection schema with optional description and relatedPosts fields and a required tags field.
    
    * Also added a default value to the date field.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,21 @@
-import { z, defineCollection } from 'astro:content';
+import { z, defineCollection, reference } from 'astro:content';
 
 const blogCollection = defineCollection({
   type: 'content',
   schema: z.object({
     title: z.string(),
+    description: z.string().optional(),
     author: z.string().default("Anonymous"),
     language: z.enum(['vi', 'en']),
-    date: z.string().transform((str) => new Date(str)),
+    date: z.string().transform((str) => new Date(str)).default((new Date()).toISOString()),
+
     bannerImage: z.object({
       src: z.string(),
       alt: z.string(),
     }).optional(),
+
+    tags: z.array(z.string()),
+    relatedPosts: z.array(reference('blog')).optional(),
   })
 });
 
