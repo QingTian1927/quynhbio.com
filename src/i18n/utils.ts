a1ea9def0417a385243fb61cbc15d83d3d432642
QingTian <qingtian63910@proton.me>
2024-04-29 10:28:39 +0700

    chore: converted space indentation to tabs
    Why? For consistency, I suppose...

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,18 +1,18 @@
 import { ui, defaultLang, wordOrder } from './ui';
 
 export function getLangFromUrl(url: URL) {
-    const [_, lang] = url.pathname.split('/');
+	const [_, lang] = url.pathname.split('/');
 
-    if (lang in ui) { return lang as keyof typeof ui; }
-    return defaultLang;
+	if (lang in ui) { return lang as keyof typeof ui; }
+	return defaultLang;
 }
 
 export function useTranslations(lang: keyof typeof ui) {
-    return function translate(key: keyof typeof ui[typeof defaultLang]) {
-        return ui[lang][key] || ui[defaultLang][key];
-    }
+	return function translate(key: keyof typeof ui[typeof defaultLang]) {
+		return ui[lang][key] || ui[defaultLang][key];
+	}
 }
 
 export function getWordOrder(lang: keyof typeof wordOrder) {
-    return wordOrder[lang] || wordOrder[defaultLang];
+	return wordOrder[lang] || wordOrder[defaultLang];
 }
