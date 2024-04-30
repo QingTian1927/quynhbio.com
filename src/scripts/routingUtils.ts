3f860cc90f5e2f0c0c98c6d32160a7d332a9ddd1
QingTian <qingtian63910@proton.me>
2024-04-30 18:01:18 +0700

    refactor: moved a function.

--- src/scripts/routingUtils.ts
+++ src/scripts/routingUtils.ts
@@ -7,3 +7,14 @@ export function getOriginalForm(link: string) {
 	if (!link) { return link; }
 	return link.replaceAll(/-/g, " ");
 }
+
+export function chunkArray(array: any[], chunkSize: number) {
+	if (chunkSize <= 0) { return undefined; }
+	if (chunkSize === array.length) { return array; }
+
+	const chunks = [];
+	for (let i = 0; i < array.length; i += chunkSize) {
+		chunks.push(array.slice(i, i + chunkSize));
+	}
+	return chunks;
+}
